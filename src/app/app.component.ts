d47e6a6b7fd47166a6d77581d4117cb341e6019a
LukasKlammer <klammer.lukas@hotmail.com>
2022-05-23 13:40:50 +0200

    zoom image, change images by js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'lukasklammer.com';
   // hier kann JavaScript eingefügt werden
+  images = ['innsbruck.jpg', 'it.jpg', 'science.jpg'];
+  currentImage = 0;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+    }, 8000);
+
+  }
 }
